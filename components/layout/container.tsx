07a7ee19d5304cb957f960e0c94f291c539bf7b0
Konstantin Gerasimenko <konstantingerasimenko789@gmail.com>
2024-07-05 20:32:18 +0300

    feat: add container

---
+++ components/layout/container.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+import { cn } from '@/utils/css'
+
+interface containerProps {
+  className?: string
+  children: ReactNode
+}
+
+export default function Container({className,children}:containerProps) {
+  return (
+    <div
+      className={cn(
+        //size
+        'max-w-[77.5rem]',
+        //position
+        'mx-auto px-4',
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
